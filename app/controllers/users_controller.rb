dcf6411be31f2f53d5dc34ff9f30a04c133b8eb8
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-05-13 12:02:25 +0900

    compleate-devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
+      flash[:notice] = "ユーザー情報を更新しました。"
       redirect_to root_path
     else
       render :edit
