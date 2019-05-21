40253f19cf98a84e87da77cadf4aac85fe7568d3
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-05-21 11:45:33 +0900

    create-message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,33 @@
+$(function() {
+  function buildHTML(message){
+    var html = 
+    `<div class="group-main__messages">
+        <p class="group-main__messages__user-name">${message.name}</p>
+        <p class="group-main__messages__date">${message.created_at}</p>
+        <p class="group-main__messages__message">${message.body}</p>
+    </div>`
+    return html;
+  }
+  $('.message_send').submit(function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "post",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.group-main').append(html)
+      $('.footer-form__message__input').val('')
+    })
+
+    .always(() => {
+      $(".footer-form__send").removeAttr("disabled");
+      });
+  })
+})
