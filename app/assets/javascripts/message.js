60189abe98e49c65cafb5c87cfd2e9aca241c9e3
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-05-21 15:43:48 +0900

    Revert "edit-message.js-if__image"
    This reverts commit 3e7af0cdb7b39934616ac1c321235e27c329986d.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,23 +4,7 @@ $(function() {
     `<div class="group-main__messages">
         <p class="group-main__messages__user-name">${message.name}</p>
         <p class="group-main__messages__date">${message.created_at}</p>
-        <div class="group-main__messages__bottom">
-          <p class="group-main__messages__bottom__message">${message.body}</p>
-        </div>
-    </div>`
-    return html;
-  }
-  function buildHTML2(message){
-    var html = 
-    `<div class="group-main__messages">
-        <p class="group-main__messages__user-name">${message.name}</p>
-        <p class="group-main__messages__date">${message.created_at}</p>
-        <div class="group-main__messages__bottom">
-          <p class="group-main__messages__bottom__message">${message.body}</p>
-          <p class="group-main__messages__bottom__image">
-            <image src='${message.image.url}' ></image>
-          </p>
-        </div>
+        <p class="group-main__messages__message">${message.body}</p>
     </div>`
     return html;
   }
@@ -28,12 +12,6 @@ $(function() {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    var image =$('.image__form').val();
-    if (image == "") {
-      load_html = buildHTML
-    } else {
-      load_html = buildHTML2
-    }
     $.ajax({
       url: url,
       type: "post",
@@ -43,14 +21,10 @@ $(function() {
       contentType: false
     })
     .done(function(data){
-      var html = load_html(data);
+      var html = buildHTML(data);
       $('.group-main').append(html)
       $('.footer-form__message__input').val('')
-      $('.image__form').val('')
       $('.group-main').animate({scrollTop: $('.group-main')[0].scrollHeight}, '500')
-    })
-    .fail(function(){
-      alert("送信に失敗しました");
     })
 
     .always(() => {
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,3 @@
 json.body @message.body
 json.name @message.user.name
-json.created_at @message.created_at.in_time_zone('Tokyo').strftime('%Y/%m/%d %H:%M')
-json.image @message.image
+json.created_at @message.created_at
