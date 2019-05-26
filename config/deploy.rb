b203629aff6efefceca367b98d2df44f486ac4b4
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-05-26 09:13:27 +0900

    aaaa

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,10 @@ set :repo_url,  'git@github.com:rooji1123/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/1899220721.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/1899220721.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
