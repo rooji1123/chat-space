8566d41e9652767d3f7072ffddbe7a63029ae6b6
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-05-26 09:11:13 +0900

    aaaa

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,16 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+# config valid only for current version of Capistrano
+lock '3.11.0'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:rooji1123/chat-space.git"
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:rooji1123/chat-space.git'
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 set :ssh_options, auth_methods: ['publickey'],
-  keys: ['~/.ssh/1899220721.pem']
+                  keys: ['~/.ssh/1899220721.pem']  ※例：~/.ssh/key_pem.pem
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
